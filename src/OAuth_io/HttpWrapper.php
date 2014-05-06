31449742b378233021d4f71586b5ec260116f693
Antoine Jackson <a.j.william26@gmail.com>
2014-05-06 18:21:46 +0200

    fixed encoding problem from http responses

--- src/OAuth_io/HttpWrapper.php
+++ src/OAuth_io/HttpWrapper.php
@@ -1,46 +1,54 @@
 <?php
-
 namespace OAuth_io;
 
-
-
 class HttpWrapper {
-	public function __create() {
+    public function __create() {
+    }
+    
+    private function array_map_recursive($callback, $array) {
+        foreach ($array as $key => $value) {
+            if (is_object($array[$key])) {
+                $array[$key] = array_map_recursive($callback, $array[$key]);
+            } else {
+                $array[$key] = call_user_func($callback, $array[$key]);
+            }
+        }
+        return $array;
+    }
+    
+    public function make_request($options) {
+        $injector = Injector::getInstance();
+        
+        $url = $options['url'];
+        $method = $options['method'];
+        $headers = $options['headers'];
+        $body = isset($options['body']) ? $options['body'] : '';
+        $response = null;
+        if (isset($options['qs'])) {
+            $qs = http_build_query($options['qs']);
+            $url.= '?' . $qs;
+        }
+        \Unirest::verifyPeer($injector->ssl_verification);
+        if ($options['method'] == 'GET') {
+            $response = \Unirest::get($url, $headers);
+        }
+        
+        if ($options['method'] == 'POST') {
+            $response = \Unirest::post($url, $headers, $body);
+        }
+        
+        if ($options['method'] == 'PUT') {
+            $response = \Unirest::put($url, $headers, $body);
+        }
+        
+        if ($options['method'] == 'DELETE') {
+            $response = \Unirest::delete($url, $headers);
+        }
+        
+        if ($options['method'] == 'PATCH') {
+            $response = \Unirest::patch($url, $headers, $body);
+        }
 
-	}
-
-	public function make_request($options) {
-		$injector = Injector::getInstance();
-
-		$url = $options['url'];
-		$method = $options['method'];
-		$headers = $options['headers'];
-		$body = isset($options['body']) ? $options['body'] : '';
-		$response = null;
-		if (isset($options['qs'])) {
-			$qs = http_build_query($options['qs']);
-			$url .= '?' . $qs;
-		}
-		\Unirest::verifyPeer($injector->ssl_verification);
-		if ($options['method'] == 'GET') {
-			$response = \Unirest::get($url, $headers);
-		}
-
-		if ($options['method'] == 'POST') {
-			$response = \Unirest::post($url, $headers, $body);
-		}
-
-		if ($options['method'] == 'PUT') {
-			$response = \Unirest::put($url, $headers, $body);
-		}
-
-		if ($options['method'] == 'DELETE') {
-			$response = \Unirest::delete($url, $headers);
-		}
-
-		if ($options['method'] == 'PATCH') {
-			$response = \Unirest::patch($url, $headers, $body);
-		}
-		return $response;
-	}
+        return $response;
+    }
 }
