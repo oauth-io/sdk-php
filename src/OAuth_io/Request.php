fffbc7464bd6474c592a0cd04d7457e55251c3d2
Antoine Jackson <a.j.william26@gmail.com>
2014-05-15 23:06:29 +0200

    added deep transformation of objects to array in request responses

--- src/OAuth_io/Request.php
+++ src/OAuth_io/Request.php
@@ -14,6 +14,10 @@ class Request {
         $this->provider = $provider;
     }
     
+    private function object_to_array($obj) {
+        return json_decode(json_encode($obj), true);
+    }
+    
     private function makeRequest($method, $url, $body_fields = null) {
         $response = null;
         if (!isset($this->injector->session['oauthio']['auth'][$this->provider])) {
@@ -21,13 +25,13 @@ class Request {
         } else {
             $prov_data = $this->injector->session['oauthio']['auth'][$this->provider];
             $requester = $this->injector->getRequest();
-
+            
             $tokens = array();
             
             $headers = array(
                 'k' => $this->injector->config['app_key']
             );
-
+            
             if (isset($prov_data['access_token'])) {
                 $headers['access_token'] = $prov_data['access_token'];
             }
@@ -36,30 +40,32 @@ class Request {
                 $headers['oauth_token_secret'] = $prov_data['oauth_token_secret'];
                 $headers['oauthv1'] = '1';
             }
-
+            
             $response = $requester->make_request(array(
                 'method' => $method,
-                'url' => $this->injector->config['oauthd_url'] . '/request/' . $this->provider . '/' . urlencode($url),
-                'headers' => array('oauthio' => http_build_query($headers)),
+                'url' => $this->injector->config['oauthd_url'] . '/request/' . $this->provider . '/' . urlencode($url) ,
+                'headers' => array(
+                    'oauthio' => http_build_query($headers)
+                ) ,
                 'body' => is_array($body_fields) ? $body_fields : null
             ));
         }
         return $response;
     }
-
+    
     private function makeMeRequest($filters) {
         if (!isset($this->injector->session['oauthio']['auth'][$this->provider])) {
             throw new \Exception('Error');
         } else {
             $prov_data = $this->injector->session['oauthio']['auth'][$this->provider];
             $requester = $this->injector->getRequest();
-
+            
             $tokens = array();
             
             $headers = array(
                 'k' => $this->injector->config['app_key']
             );
-
+            
             if (isset($prov_data['access_token'])) {
                 $headers['access_token'] = $prov_data['access_token'];
             }
@@ -68,18 +74,19 @@ class Request {
                 $headers['oauth_token_secret'] = $prov_data['oauth_token_secret'];
                 $headers['oauthv1'] = '1';
             }
-
-
+            
             if (is_array($filters)) {
                 $filters = array(
                     'filter' => join(',', $filters)
                 );
             }
-
+            
             $response = $requester->make_request(array(
                 'method' => 'GET',
                 'url' => $this->injector->config['oauthd_url'] . '/auth/' . $this->provider . '/me',
-                'headers' => array('oauthio' => http_build_query($headers)),
+                'headers' => array(
+                    'oauthio' => http_build_query($headers)
+                ) ,
                 'qs' => is_array($filters) ? $filters : null
             ));
         }
@@ -87,27 +94,33 @@ class Request {
     }
     
     public function get($url) {
-        return (array) $this->makeRequest('GET', $url)->body;
+        $response = $this->makeRequest('GET', $url)->body;
+        $response = $this->object_to_array($response);
+        return $response;
     }
     
     public function post($url, $fields) {
-        return (array) $this->makeRequest('POST', $url, $fields)->body;
+        $response = $this->makeRequest('POST', $url, $fields)->body;
+        return $this->object_to_array($response);
     }
     
     public function put($url, $fields) {
-        return (array) $this->makeRequest('PUT', $url, $fields)->body;
+        $response = $this->makeRequest('PUT', $url, $fields)->body;
+        return $this->object_to_array($response);
     }
     
     public function del($url) {
-        return (array) $this->makeRequest('DELETE', $url)->body;
+        $response = $this->makeRequest('DELETE', $url)->body;
+        return $this->object_to_array($response);
     }
     
     public function patch($url, $fields) {
-        return (array) $this->makeRequest('PATCH', $url, $fields)->body->data;
+        $response = $this->makeRequest('PATCH', $url, $fields)->body->data;
+        return $this->object_to_array($response);
     }
-
-    public function me($filters=null) {
-        $body = $this->makeMeRequest($filters)->body->data;
-        return (array) $body;
+    
+    public function me($filters = null) {
+        $response = $this->makeMeRequest($filters)->body->data;
+        return $this->object_to_array($response);
     }
 }
